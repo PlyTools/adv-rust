79fcb8a114ff68f7de42cc952c1fd7c9632c0d9c
PlyTruth <qianren1024@gmail.com>
2023-02-16 16:03:42 +0000

    feat: add examples for testing

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,21 @@
+//! Copyright (c) Liam 2022
+//! All rights reserved. Licensed under MIT.
+//!
+//! The examples in Chapter 10.3
+//!
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
