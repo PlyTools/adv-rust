a8bdfd96c042839b42d5d1495f523f2cf1701533
PlyTruth <qianren1024@gmail.com>
2023-02-24 21:01:41 +0000

    feat: add examples of combing Rc<T> and RefCell<T>

---
+++ rct_refcelt/src/main.rs
@@ -0,0 +1,30 @@
+//! Copyright (c) Liam 2023
+//! All rights reserved under MIT license.
+//! 
+//! Examples of combing Rc<T> and RefCel<T> in Chapter 15.5
+
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
