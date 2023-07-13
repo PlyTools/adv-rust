d80ed1d589a461c52a28277af261bd28199e2848
PlyTruth <qianren1024@gmail.com>
2023-07-13 10:55:06 +0800

    test: add unit test of wallet

--- blockchain/bip44/src/wallet.rs
+++ blockchain/bip44/src/wallet.rs
@@ -44,3 +44,17 @@ impl Wallet {
 
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_wallet() {
+        let phrase = "work man father plunge mystery proud hollow address reunion sauce theory bonus";
+        let index = 0u32;
+        let password = "TREZOR123";
+
+        let wallet = Wallet::new(phrase, index, password);
+    }
+}
+
